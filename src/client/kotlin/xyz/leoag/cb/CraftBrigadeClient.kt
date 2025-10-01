2ee0d12f9273262e6fd303ddbcd422bf274f5b34
Leonardo G. <leoagomes@users.noreply.github.com>
2025-10-01 02:17:46 -0300

    bootstrap project

---
+++ src/client/kotlin/xyz/leoag/cb/CraftBrigadeClient.kt
@@ -0,0 +1,9 @@
+package xyz.leoag.cb
+
+import net.fabricmc.api.ClientModInitializer
+
+object CraftBrigadeClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
