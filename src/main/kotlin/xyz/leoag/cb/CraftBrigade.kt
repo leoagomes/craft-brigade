0e9712c9fc0747cac48e44c1761b4eda56e05934
Leonardo G. <leoagomes@users.noreply.github.com>
2025-10-01 02:19:45 -0300

    clean up

--- src/main/kotlin/xyz/leoag/cb/CraftBrigade.kt
+++ src/main/kotlin/xyz/leoag/cb/CraftBrigade.kt
@@ -4,12 +4,10 @@ import net.fabricmc.api.ModInitializer
 import org.slf4j.LoggerFactory
 
 object CraftBrigade : ModInitializer {
-    private val logger = LoggerFactory.getLogger("craft-brigade")
+    const val MOD_ID = "craft-brigade"
+    private val logger = LoggerFactory.getLogger(MOD_ID)
 
 	override fun onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		logger.info("Hello Fabric world!")
+		logger.info("onInitialize called")
 	}
 }
